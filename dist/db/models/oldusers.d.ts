c818c1013d8c4dac40ce60b4f3b713cf925dba46
exception <125664448+NoException-exe@users.noreply.github.com>
2024-07-22 16:30:49 -0300

    Delete dist directory

--- dist/db/models/oldusers.d.ts
+++
@@ -1,4 +0,0 @@
-import { TUser } from "../../types/types";
-export declare const OldUser: import("mongoose").Model<TUser, {}, {}, {}, import("mongoose").Document<unknown, {}, TUser> & TUser & Required<{
-    _id: string;
-}>, any>;
